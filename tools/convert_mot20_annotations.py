c6e0723845ff98c90c056e8eff2f7c1f11c3f362
Helicopt <fwtt20071028@126.com>
2021-11-13 15:51:51 +0000

    gen test data

--- tools/convert_mot20_annotations.py
+++ tools/convert_mot20_annotations.py
@@ -1,6 +1,8 @@
 from typing import List
 import json
 import os
+import sys
+import configparser
 import cv2
 
 from senseTk.common import TrackSet, Det
@@ -18,23 +20,33 @@ def convert_annos(seqs, split, half=False, gap=30):
     half_data_1 = []
     for seq in seqs:
         seq_dir = os.path.join(DATA_ROOT, split, seq)
-        ground_truth = TrackSet(os.path.join(seq_dir, 'gt', 'gt.txt'))
+        gt_path = os.path.join(seq_dir, 'gt', 'gt.txt')
+        if os.path.exists(gt_path):
+            ground_truth = TrackSet(gt_path)
+        else:
+            ground_truth = None
         image_probe = cv2.imread(os.path.join(seq_dir, 'img1', '000001.jpg'))
         image_height, image_width, _ = image_probe.shape
-        for frame_id in ground_truth.frameRange():
+        inifile = os.path.join(seq_dir, 'seqinfo.ini')
+        iniconfig = configparser.ConfigParser()
+        iniconfig.read(inifile)
+        min_fr = 1
+        max_fr = int(iniconfig['Sequence']['seqLength'])
+        for frame_id in range(min_fr, max_fr + 1):
             img = os.path.join(seq_dir, 'img1', '%06d.jpg' % frame_id)
-            gtdets: List[Det] = ground_truth[frame_id]
             instances = []
-            for gtdet in gtdets:
-                if gtdet.status != 1:
-                    continue
-                one = {
-                    'is_ignored': False,
-                    'bbox': [gtdet.x1, gtdet.y1, gtdet.x2, gtdet.y2],
-                    'label': 1,
-                    'track_id': gtdet.uid,
-                }
-                instances.append(one)
+            if ground_truth:
+                gtdets: List[Det] = ground_truth[frame_id]
+                for gtdet in gtdets:
+                    if gtdet.status != 1:
+                        continue
+                    one = {
+                        'is_ignored': False,
+                        'bbox': [gtdet.x1, gtdet.y1, gtdet.x2, gtdet.y2],
+                        'label': 1,
+                        'track_id': gtdet.uid,
+                    }
+                    instances.append(one)
             data = {
                 'filename': os.path.abspath(img),
                 'instances': instances,
@@ -43,10 +55,10 @@ def convert_annos(seqs, split, half=False, gap=30):
                 'formatter': formatter,
             }
             all_data.append(data)
-            half = (ground_truth.min_fr + ground_truth.max_fr) // 2
-            if frame_id <= half:
+            half_ = (min_fr + max_fr) // 2
+            if frame_id <= half_:
                 half_data_0.append(data)
-            elif frame_id > half + gap:
+            elif frame_id > half_ + gap:
                 half_data_1.append(data)
     anno_dir = os.path.join(DATA_ROOT, 'annotations')
     os.makedirs(anno_dir, exist_ok=True)
@@ -60,8 +72,10 @@ def convert_annos(seqs, split, half=False, gap=30):
             fd.writelines(map(dump_row, half_data_1))
 
 
-train_seqs = os.listdir(os.path.join(DATA_ROOT, 'train'))
-convert_annos(train_seqs, 'train', half=True, gap=30)
-
-
-test_seqs = os.listdir(os.path.join(DATA_ROOT, 'test'))
+if __name__ == '__main__':
+    train_seqs = os.listdir(os.path.join(DATA_ROOT, 'train'))
+    test_seqs = os.listdir(os.path.join(DATA_ROOT, 'test'))
+    if sys.argv[1] == 'test':
+        convert_annos(test_seqs, 'test')
+    else:
+        convert_annos(train_seqs, 'train', half=True, gap=30)
