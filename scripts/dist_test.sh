715cd0f1f07b42af9531a170533b54e805f34429
qiaolei <qiaolei@sensetime.com>
2021-12-27 16:58:11 +0800

    update scripts

--- scripts/dist_test.sh
+++ scripts/dist_test.sh
@@ -1,15 +1,98 @@
 #!/bin/bash
 
-ROOT=../
 T=`date +%m%d%H%M`
-export ROOT=$ROOT
-cfg=$2
-export PYTHONPATH=$ROOT:$PYTHONPATH
+# ROOT=../../
 
-python -m eod train \
+show_help() {
+cat << EOF
+Usage:
+    ${0##*/} [-h/--help] [-p/--partition] [-n/--num] [-g/--gpu] [-c/--config] [-e/--env-name]
+EOF
+}
+
+EODROOT=/mnt/lustre/qiaolei/code/EOD/eod
+PLUGINROOT=/mnt/lustre/qiaolei/code/TRACKING/EOD-Tracking/tracking
+config=config.yaml
+partition=UCG_Share
+GPU=8
+NUM=2
+ENV_NAME=s0.3.4
+backbend=linklink
+model=model
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -h|--help)
+            show_help
+            exit
+            ;;
+        -m|--model)
+            model=$2
+            shift 2
+            ;;
+        -p|--partition)
+            partition=$2
+            shift 2
+            ;;
+        -n|--num)
+            NUM=$2
+            shift 2
+            ;;
+        -g|--gpu)
+            GPU=$2
+            shift 2
+            ;;
+        -c|--config)
+            config=$2
+            shift 2
+            ;;
+        -b|--backbend)
+            backbend=$2
+            shift 2
+            ;;
+        -e|--env-name)
+            ENV_NAME=$2
+            shift 2
+            ;;
+        *)
+            echo invalid arg [$1]
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+# source $ENV_NAME
+which python
+
+eod_env_setup() {
+    export PLUGINPATH=$PLUGINROOT
+    echo "PLUGINPATH: $PLUGINROOT"
+
+    # export EODROOT=$EODROOT
+    export PYTHONPATH=$EODROOT:$PYTHONPATH
+    echo "EODROOT: $EODROOT"
+}
+
+eod_env_setup
+
+TOTAL=`expr $GPU \* $NUM`
+cat<<EOF
+training ${config} on $partition, use $NUM node(s), $GPU gpu(s), total $TOTAL card(s).
+EOF
+
+
+spring.submit run -p $partition -n$TOTAL \
+    --job-name=$(basename `pwd`) \
+    --gpu \
+    -x SH-IDC1-10-198-8-237 \
+    --cpus-per-task 5 \
+"python -m eod train \
   -e \
-  --ng=$1  
-  --launch=pytorch \
-  --config=$cfg \
-  --display=10 \
-  2>&1 | tee log.test.$T.$(basename $cfg) 
+  --config=$config \
+  --display=1 \
+  --backend=$backbend \
+  --opts saver.resume_model $model saver.auto_resume false \
+  2>&1 | tee log.train.$T.$(basename $config) "
