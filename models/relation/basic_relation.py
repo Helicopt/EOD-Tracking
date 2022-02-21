a034f5e19e9d07e393730784b5b91839127640a5
Helicopt <fwtt20071028@126.com>
2022-02-21 13:03:37 +0000

    minor fix

--- models/relation/basic_relation.py
+++ models/relation/basic_relation.py
@@ -37,7 +37,7 @@ class TransformerRelaton(nn.Module):
         self.relation_layer = nn.Transformer(d_model=embed_dim, nhead=num_heads,
                                              num_encoder_layers=1, num_decoder_layers=1, dim_feedforward=embed_dim)
 
-    def forward(self, main_feats, ref_feats, target_main=None, target_ref=None, original_preds=None):
+    def forward(self, main_feats, ref_feats, target_main=None, target_ref=None, original_preds=None, **kwargs):
         refined_feats = self.relation_layer(
             ref_feats.permute(1, 0, 2), main_feats.permute(1, 0, 2))
         refined_feats = refined_feats.permute(1, 0, 2)
@@ -53,7 +53,7 @@ class VanillaRelaton(nn.Module):
         self.dropout = nn.Dropout(0.1)
         self.norm = nn.LayerNorm(embed_dim)
 
-    def forward(self, main_feats, ref_feats, target_main=None, target_ref=None, original_preds=None):
+    def forward(self, main_feats, ref_feats, target_main=None, target_ref=None, original_preds=None, **kwargs):
         attention, affinities = self.relation_layer(
             main_feats.permute(1, 0, 2), ref_feats.permute(1, 0, 2), ref_feats.permute(1, 0, 2))
         attention = attention.permute(1, 0, 2)
