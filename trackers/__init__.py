e2bf9fc0c4680176174c21055663dd23d0f59112
Helicopt <fwtt20071028@126.com>
2022-03-19 02:00:46 +0000

    matracker

--- trackers/__init__.py
+++ trackers/__init__.py
@@ -2,5 +2,6 @@ from .no_tracking import *  # noqa
 from .online_trackers import *  # noqa
 from .crowded_trackers import *  # noqa
 from .byte_tracker import *  # noqa
+from .byte_ma_tracker import *  # noqa
 from .fair_tracker import *  # noqa
 from .simple_tracker import *  # noqa
